5a7624ef397da3fc4bbfb89b730ffb265eb99540
xdanger <xdanger@gmail.com>
2018-09-02 11:54:45 +0800

    init commit

---
+++ cloudbuild.generator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd `dirname $0`/library
+
+echo -n 'steps:' > ../cloudbuild.yml
+
+images=`\ls`
+for dir in $images; do
+  if [[ $dir =~ ^([a-z][a-z0-9\-]+)-([0-9][\.0-9]+[0-9]+)$ ]]; then
+    image=${BASH_REMATCH[1]}
+    ver=${BASH_REMATCH[2]}
+  else
+    ver=latest
+  fi
+  echo -n "
+- name: 'gcr.io/cloud-builders/docker'
+  args: [ 'build', '-t', 'gcr.io/\$PROJECT_ID/$image:$ver', '$dir' ]" >> ../cloudbuild.yml
+done
